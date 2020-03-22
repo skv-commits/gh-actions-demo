e72b098ddb732a8b069618eee0a905cac5f8f81c
cyberbot143 <krishnasai952@gmail.com>
2020-03-22 00:16:56 -0400

    chnages

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const session = require('client-sessions')
 
 const app = express();
 
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+router.get('/sitemap', function (req, res) {
+    res.sendFile(path.join(__dirname + '/sitemap.html'));
+});
+
 app.use(express.json());
 app.use(session({
     secret: process.env.SESSION_SECRET || 'my-super-secret',
@@ -16,6 +31,8 @@ const headerAuth = require('./header-auth-mdw');
 app.post('/session', login);
 app.delete('/session', logout);
 app.get('/session', headerAuth, checkAuth);
+
+app.use('/', router);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
